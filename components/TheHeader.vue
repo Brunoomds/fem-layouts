69a8278daa5bcac3a437fcf7f3f0ba5fce023153
Brunoomds <brunoo.m.d.silva@gmail.com>
2022-10-07 22:38:36 -0300

    wip

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,11 +1,7 @@
 <template>
-	<div class="bg-indigo-600 py-8 shadow-lg">
-		<Container class="flex justify-between items-center flex-col sm:flex-row gap-4 text-white text-center">
-			<h2 class="text-4xl font-serif">Bruno Silva</h2>
-			<div class="flex flex-col sm:flex-row items-center gap-2 sm:gap-6 font-medium">
-				<p class="px-4 py-2">Portfolio (soon)</p>
-				<a href="/" class="px-4 py-2 bg-indigo-700">Frontend Mentor Projects</a>
-			</div>
+	<header class="bg-indigo-600 py-8 shadow-lg text-white text-center">
+		<Container class="flex justify-center">
+			<a href="/"><h2 class="text-4xl font-serif">Bruno Silva</h2></a>
 		</Container>
-	</div>
+	</header>
 </template>
